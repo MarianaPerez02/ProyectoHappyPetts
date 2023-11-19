d7915a8a51b3767ca5d04fcd6461c7e058233e36
mdavidrodriguez <mdavidrodriguez@unicesar.edu.co>
2023-11-19 15:51:39 -0500

    rutas

--- lib/ui/screens/app.dart
+++ lib/ui/screens/app.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:proyectomovil/ui/screens/gestionInventario/editarMedicamento.dart';
+import 'package:proyectomovil/ui/screens/gestionInventario/ingresarMedicamento/ingresarMedicamentos.dart';
 import 'package:proyectomovil/ui/screens/gestionUser/register.dart';
 import 'package:proyectomovil/ui/screens/panelDeControl/panel_De_Control.dart';
 import 'package:proyectomovil/ui/screens/home/home.dart';
@@ -17,8 +19,10 @@ class MyApp extends StatelessWidget {
       ),
       home: const Home(),
       routes: {
-        '/panelDeControl':(context) => const PanelDeControl(),
-        '/register': (context) =>  Register(),
+        '/panelDeControl': (context) => const PanelDeControl(),
+        '/register': (context) => Register(),
+        '/editarMedicamento': (context) => const EditarMedicamento(),
+        '/agregarMedicamento': (context) => const IngresarMedicamento(),
       },
     );
   }
